323bec5562d485e57318d412f5186a011917650e
eastseven <eastseven@foxmail.com>
2017-07-19 08:43:44 +0800

    metronic init

---
+++ metronic-responsive-admin-dashboard-template/src/main/java/cn/eastseven/metronic/MetronicResponsiveAdminDashboardTemplateApplication.java
@@ -0,0 +1,12 @@
+package cn.eastseven.metronic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetronicResponsiveAdminDashboardTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetronicResponsiveAdminDashboardTemplateApplication.class, args);
+	}
+}
