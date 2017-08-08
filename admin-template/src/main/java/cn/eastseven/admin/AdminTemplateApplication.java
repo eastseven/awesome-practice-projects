c8caa0d139801c3a81fee4ac9adb3b2e69b9c47b
eastseven <eastseven@foxmail.com>
2017-08-08 18:37:03 +0800

    ace admin layout ajax

--- admin-template/src/main/java/cn/eastseven/admin/AdminTemplateApplication.java
+++ admin-template/src/main/java/cn/eastseven/admin/AdminTemplateApplication.java
@@ -3,18 +3,29 @@ package cn.eastseven.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @SpringBootApplication
 public class AdminTemplateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminTemplateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdminTemplateApplication.class, args);
+    }
 
-	@GetMapping("/")
-	public String index() {
-		return "gentelella/index";
-	}
+    @GetMapping("/")
+    public String index() {
+        return "gentelella/index";
+    }
+
+    @GetMapping(value = {"/ace", "/ace/index.html"})
+    public String index(Model model) {
+        return "ace/index";
+    }
+
+    @GetMapping("/ace/hello.html")
+    public String hello() {
+        return "ace/hello";
+    }
 }
