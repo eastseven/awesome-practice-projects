dd071cad1ad65eba1f7cd2a7fdac380c805b0d73
eastseven <eastseven@foxmail.com>
2017-10-11 13:36:45 +0800

    通过正则表达式，剔除html标签中的属性

--- web-crawler/src/test/java/cn/eastseven/webcrawler/HtmlTagFormatTests.java
+++ web-crawler/src/test/java/cn/eastseven/webcrawler/HtmlTagFormatTests.java
@@ -17,7 +17,7 @@ public class HtmlTagFormatTests {
 
     @Test
     public void test() throws IOException {
-        String url = "http://www.ggzy.gov.cn/information/html/b/350000/0201/201709/26/00350f7009fcf67d43db8f680e6469ea984f.shtml";
+        String url = "http://www.ggzy.gov.cn/html/b/330000/0202/201710/10/003341f5fc201f1742b2967dee9be8e17c72.shtml";
         String css = "div#mycontent div.detail_content";
 
         Elements content = Jsoup.connect(url).get().body().select(css).first().children();
@@ -42,6 +42,9 @@ public class HtmlTagFormatTests {
     public void testRecursion() throws Exception {
         String url = "http://zhaobiao.wubaiyi.com/show-7-318052-1.html";
         String css = "#Article div.content";
+
+        url = "http://www.chinaunicombidding.cn/jsp/cnceb/web/info1/detailNotice.jsp?id=2875703300000009139";
+        css = "div#mycontent div.detail_content";
 
         Element root = Jsoup.connect(url).get().body().select(css).first();
         Assert.assertNotNull(root);
@@ -71,5 +74,28 @@ public class HtmlTagFormatTests {
                 visit(child, lines);
             }
         }
+    }
+
+    @Test
+    public void testHtmlTagReplace() throws Exception {
+        String url = "http://www.chinaunicombidding.cn/jsp/cnceb/web/info1/detailNotice.jsp?id=2875703300000009139";
+        String css = "body > div.Section1";
+        url = "http://www.ggzy.gov.cn/information/html/b/660000/0204/201710/11/0066424ede6b6d994efa9c01f069190693c8.shtml";
+        css = "div.detail_content";
+
+        //url = "http://empm.ccccltd.cn/PMS/biddetail.shtml?id=J8Gis7a8zFp0/0+cu62h4ufCdjRQ/t9M5buuVsVwZblCKGlOcrU5SpArq7je03IAqGcjcm4LS06Jg7x4SlJl5mGjrLcESjEnqGcjcm4LS06wunPuBDym2zkNGvz9XSmH";
+        //css = "body > div > table";
+
+        Element body = Jsoup.connect(url).get().body();
+        Assert.assertNotNull(body);
+
+        String html = body.select(css).html();
+        Assert.assertNotNull(html);
+        System.out.println(" ===== 原文 =====");
+        System.out.println(html);
+
+        String text = StringUtils.removeAll(html, "\\w+=\".*?\"|<!-{2,}.*?-{2,}>");
+        System.out.println(" ===== 处理后 =====");
+        System.out.println(text);
     }
 }
