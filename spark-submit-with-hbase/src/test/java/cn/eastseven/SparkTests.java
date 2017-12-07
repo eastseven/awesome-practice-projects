3d6bf65fa1a04401d97fc6c9ca7c09a64ba01f39
eastseven <eastseven@foxmail.com>
2017-12-07 15:03:06 +0800

    modify spark demo

--- spark-submit-with-hbase/src/test/java/cn/eastseven/SparkTests.java
+++ spark-submit-with-hbase/src/test/java/cn/eastseven/SparkTests.java
@@ -3,8 +3,7 @@ package cn.eastseven;
 import cn.eastseven.config.HBaseConfig;
 import cn.eastseven.handler.BidNewOriginalTableHandler;
 import com.google.common.collect.Lists;
-import com.hankcs.hanlp.seg.CRF.CRFSegment;
-import com.hankcs.hanlp.seg.Segment;
+import com.google.common.collect.Sets;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
@@ -17,6 +16,13 @@ import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
 import org.junit.Assert;
@@ -25,6 +31,7 @@ import scala.Tuple2;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -134,38 +141,55 @@ public class SparkTests extends ParentTests {
         HBaseConfig conf = new HBaseConfig();
         final byte[] f = Bytes.toBytes("f");
         final byte[] original_id = Bytes.toBytes("original_id");
+        final byte[] create_time = Bytes.toBytes("create_time");
         final byte[] category = Bytes.toBytes("category");
         final byte[] key_words = Bytes.toBytes("key_words");
         final byte[] title = Bytes.toBytes("title");
+
+        FilterList filterList = new FilterList(FilterList.Operator.MUST_PASS_ALL);
+        filterList.addFilter(new SingleColumnValueFilter(f, original_id, CompareFilter.CompareOp.EQUAL, new SubstringComparator("201712")));
+        //filterList.addFilter(new SingleColumnValueFilter(f, category, CompareFilter.CompareOp.EQUAL, new SubstringComparator("其他")));
         Scan scan = new Scan();
         scan.addColumn(f, category);
         scan.addColumn(f, title);
-        //scan.addColumn(f, key_words);
-        scan.setFilter(new SingleColumnValueFilter(f, original_id, CompareFilter.CompareOp.EQUAL, new SubstringComparator("20171130")));
+        scan.addColumn(f, key_words);
+        scan.addColumn(f, original_id);
+
+        scan.setFilter(filterList);
         Configuration hConf = conf.createNew(TableName.valueOf("bid_news", "bid_news_analysis"), scan);
         Class fClass = TableInputFormat.class;
         Class kClass = ImmutableBytesWritable.class;
         Class vClass = Result.class;
-        JavaPairRDD<ImmutableBytesWritable, Result> rdd = jsc.newAPIHadoopRDD(hConf, fClass, kClass, vClass).cache();
+        JavaPairRDD<ImmutableBytesWritable, Result> rdd = jsc.newAPIHadoopRDD(hConf, fClass, kClass, vClass);
         long total = rdd.count();
 
-        JavaRDD<String> resultRDD = rdd.map(row -> {
-            String titleValue = Bytes.toString(row._2.getValue(f, title));
-            String categoryValue = Bytes.toString(row._2.getValue(f, category));
-
-            String seg = "@";
-            if (StringUtils.isNotBlank(titleValue)) {
-                Segment segment = new CRFSegment();
-                segment.enablePartOfSpeechTagging(true);
-                seg = segment.seg(titleValue).stream().map(term -> term.word).distinct().collect(Collectors.toSet()).toString();
-            }
-
-            return String.join(" | ", titleValue, seg, categoryValue);
-        });
+        JavaRDD<Row> resultRDD = rdd.filter(row -> StringUtils.isNotBlank(Bytes.toString(row._2.getValue(f, key_words))))
+                .filter(row -> StringUtils.startsWith(Bytes.toString(row._2.getValue(f, original_id)), "201712"))
+                .map(row -> {
+                    String id = Bytes.toString(row._2.getValue(f, original_id));
+                    String titleValue = Bytes.toString(row._2.getValue(f, title));
+                    String keywords = Bytes.toString(row._2.getValue(f, key_words));
+                    String categoryValue = Bytes.toString(row._2.getValue(f, category));
+                    Set<String> set = Sets.newHashSet(keywords.split(" ")).stream()
+                            .filter(k -> StringUtils.strip(k).length() > 0).collect(Collectors.toSet());
+                    keywords = StringUtils.substringBetween(set.toString(), "[", "]");
+                    //return String.join(",", categoryValue, titleValue, keywords);
+                    return RowFactory.create(categoryValue, id, titleValue, keywords, Bytes.toString(row._2.getRow()));
+                });
         long result = resultRDD.count();
-
         log.info(">>> total {}, result {}", total, result);
-        resultRDD.saveAsTextFile("target/title_category_" + DateTime.now().toString("yyyy_MM_dd_HH_mm"));
+
+        List<StructField> fieldList = Lists.newArrayList();
+        fieldList.add(DataTypes.createStructField("category", DataTypes.StringType, true));
+        fieldList.add(DataTypes.createStructField("original_id", DataTypes.StringType, true));
+        fieldList.add(DataTypes.createStructField("title", DataTypes.StringType, true));
+        fieldList.add(DataTypes.createStructField("keywords", DataTypes.StringType, true));
+        fieldList.add(DataTypes.createStructField("rowkey", DataTypes.StringType, true));
+        StructType schema = DataTypes.createStructType(fieldList);
+        SQLContext sql = SQLContext.getOrCreate(jsc.sc());
+
+        Dataset<Row> df = sql.createDataFrame(resultRDD, schema);
+        df.write().csv("target/title_category_" + DateTime.now().toString("yyyy_MM_dd_HH_mm"));
     }
 
     @Test
@@ -211,5 +235,27 @@ public class SparkTests extends ParentTests {
             log.info(">>> {} table {} total {}", date, original.getNameWithNamespaceInclAsString(), total);
         }
 
+    }
+
+    @Test
+    public void testCountOriginalTableDailyBySourceCode() throws Exception {
+        TableName original = TableName.valueOf("bid_news", "bid_news_original");
+
+        String day = "20171206";
+        byte[] f = Bytes.toBytes("f");
+        byte[] sourceCode = Bytes.toBytes("sourceCode");
+        Scan scan = new Scan();
+        scan.addColumn(f, sourceCode);
+        scan.setFilter(new PrefixFilter(Bytes.toBytes(day)));
+
+        JavaPairRDD<ImmutableBytesWritable, Result> rdd = loadData(original, scan);
+        long total = rdd.count();
+        log.info(">>> table {} total {}", original.getNameWithNamespaceInclAsString(), total);
+
+        JavaPairRDD<String, String> result = rdd.mapToPair(row -> new Tuple2<>(Bytes.toString(row._2.getValue(f, sourceCode)), Bytes.toString(row._2.getRow())));
+        result.count();
+
+        List<String> data = result.groupByKey().map(row -> row._1 + "=" + Lists.newArrayList(row._2).size()).cache().collect();
+        data.forEach(System.out::println);
     }
 }
